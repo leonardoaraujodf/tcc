79c13182d423cff2bf9ecbe8262d36f6c414816a
Leonardo Amorim <leonardoaraujodf@gmail.com>
2020-11-28 13:52:31 -0200

    Creating way to plot all data
       - There was a bug in the last implementation related with the
       time arrays. They weren't being treated correctly, and the time
       arrays were mismatching with the samples array. This was solved
       now.

--- code/fft_python/ppg_extraction.py
+++ code/fft_python/ppg_extraction.py
@@ -4,8 +4,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from copy import deepcopy
-import matplotlib
-matplotlib.use('TkAgg')
 # ---}}}
 
 def get_ppg_info():
@@ -84,9 +82,29 @@ def ppg_extract_data():
          ppg_data = ppg_temp
       else:
          ppg_data.ppg_vstack(ppg_temp)
+   
+   ppg_data.generate_time_axis()
 
    return ppg_data
 
+def plot_ppg_data_in_time(ppg_data):
+   import matplotlib
+   matplotlib.use('TkAgg')
+
+   print(ppg_data.x.shape)
+   print(ppg_data.x_rest.shape)
+   print(ppg_data.x_finger.shape)
+   plt.plot(ppg_data.t_rest, ppg_data.x_rest[:, 0])
+   plt.xlabel('Tempo $t$ (seconds)')
+   plt.ylabel('$x_c(t)$ (volts)')
+   plt.title('Channel N. 0 (rest)')
+   plt.show()
+   plt.plot(ppg_data.t_finger, ppg_data.x_finger[:, 0])
+   plt.xlabel('Tempo $t$ (seconds)')
+   plt.ylabel('$x_c(t)$ (volts)')
+   plt.title('Channel N. 0 (rest)')
+   plt.show()
+    
 class PpgData:
    def __init__(self, filename, rest_beginning_seconds, rest_end_seconds, finger_beginning_seconds, \
                 finger_end_seconds, n_detectors = 5, n_sources = 2, total_fs = 4500, ad_converter_voltage = 3.3, \
@@ -107,9 +125,11 @@ class PpgData:
       self.t_finger = []
       self.x = []
       self.t_total = []
+      self.n_channels = []
+      self.fs = []
 
    def ppg_extraction(self):
-      n_channels = self.n_sources * self.n_detectors
+      self.n_channels = self.n_sources * self.n_detectors
       with open(self.filename, 'r') as fp:
          hexvals = fp.readlines()
 
@@ -117,30 +137,36 @@ class PpgData:
       decvals = np.array(decvals)
       raw_signal = (self.ad_converter_voltage/self.ad_converter_resolution) * decvals
       raw_signal = raw_signal - np.mean(raw_signal)
-      raw_signal = raw_signal
-      m = len(raw_signal) % n_channels
+      m = len(raw_signal) % self.n_channels
       if m != 0:
          raw_signal = raw_signal[:-m]
-      t_total = np.arange(len(raw_signal))
-      t_total = (1/self.total_fs) * t_total
-      N = int(len(raw_signal) / n_channels)
-      self.x = raw_signal.reshape(N, n_channels)
-      fs = self.total_fs / n_channels
-      self.x_rest = deepcopy(self.x[int(self.rest_beginning_seconds * fs) : int(self.rest_end_seconds * fs), :])
-      self.x_finger = deepcopy(self.x[int(self.finger_beginning_seconds * fs) : int(self.finger_end_seconds * fs), :])
-      self.t_rest = n_channels * deepcopy(t_total[int(self.rest_beginning_seconds * fs) : int(self.rest_end_seconds * fs)])
-      self.t_finger = n_channels * deepcopy(t_total[int(self.finger_beginning_seconds * fs) : \
-                                 int(self.finger_end_seconds * fs)])
+
+# Only generate a time array which has the same lenght of the signal.
+# When we finish processing all data, then use generate_time_axis to really generate the time array.
+      self.t_total = np.arange(len(raw_signal))
+
+      N = int(len(raw_signal) / self.n_channels)
+      self.x = raw_signal.reshape(N, self.n_channels)
+      self.fs = self.total_fs / self.n_channels
+      self.x_rest = deepcopy(self.x[int(self.rest_beginning_seconds * self.fs) : int(self.rest_end_seconds * self.fs), :])
+      self.x_finger = deepcopy(self.x[int(self.finger_beginning_seconds * self.fs) : int(self.finger_end_seconds * self.fs), :])
+
+   def generate_time_axis(self):
+      self.t_total = (1/self.total_fs) * self.t_total
+      self.t_rest = self.n_channels * deepcopy(self.t_total[int(self.rest_beginning_seconds * self.fs) : int(self.rest_end_seconds * self.fs)])
+      self.t_finger = self.n_channels * deepcopy(self.t_total[int(self.finger_beginning_seconds * self.fs) : \
+                                 int(self.finger_end_seconds * self.fs)])
+
+   def adjust_time_samples(self, ppg_data):
+      self.t_total = np.hstack((self.t_total, ppg_data.t_total))
+      self.rest_end_seconds += ppg_data.rest_end_seconds - ppg_data.rest_beginning_seconds
+      self.finger_end_seconds += ppg_data.finger_end_seconds - ppg_data.finger_beginning_seconds
 
    def ppg_vstack(self, ppg_data):
-      print(self.x_rest.shape)
-      print(ppg_data.x_rest.shape)
       self.x_rest = np.vstack((self.x_rest, ppg_data.x_rest))
       self.x_finger = np.vstack((self.x_finger, ppg_data.x_finger))
-      self.t_rest = np.vstack((self.t_rest, ppg_data.t_rest))
-      self.t_finger = np.vstack((self.t_finger, ppg_data.t_finger))
       self.x = np.vstack((self.x, ppg_data.x))
-      self.t_total = np.vstack((self.t_total, ppg_data.t_total))
+      self.adjust_time_samples(ppg_data)
 
 '''
 # --- ppg_extraction -------------------------------------------------------{{{
